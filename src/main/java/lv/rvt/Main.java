5aefe212a9520407e2841c4f13e534507b83ff8c
Renārs Pilāns <166484724+23DP4RPila@users.noreply.github.com>
2025-03-18 07:54:05 +0000

    Pievienoju galveno izvelni un pirksanas izvelni

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,11 +1,38 @@
 package lv.rvt;
 
 import java.util.*;
+import java.util.Scanner;
+import java.nio.file.Paths;
+import java.nio.file.Paths;
 
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        // Make your first Hello World!
-    }
+public class Main {
+
+public static void main(String[] args)
+{   
+Scanner izvele = new Scanner(System.in);
+int izveletais = 1;
+String izvelne = "galvena";
+if (izvelne == "galvena")     {;
+    System.out.println("==========GALVENĀ IZVĒLNE==========");
+    System.out.println();
+    System.out.println("Izvēlieties vienu no šīm opcijām: ");
+    System.out.println();
+    System.out.println("(1) Rādīt tirdzniecības automāta saturu");
+    System.out.println("(2) Pirkt");
+    System.out.println("(3) Iziet");
+    System.out.println();
+    System.out.println("Rakstiet vienu no šiem cipariem, lai veiktu atbilstošo darbību: ");
+}
+if (izveletais == 1) {
+    System.out.println("==========PIRKŠANAS IZVĒLNE==========");
+    System.out.println();
+    System.out.println("Izvēlieties vienu no šīm opcijām: ");
+    System.out.println();
+    System.out.println("(1) Pievienot naudu");
+    System.out.println("(2) Izvēlēties produktu");
+    System.out.println("(3) Atpakaļ uz galveno izvēlni");
+    System.out.println();
+    System.out.println("Rakstiet vienu no šiem cipariem, lai veiktu atbilstošo darbību: ");
+}
+ }
 }
